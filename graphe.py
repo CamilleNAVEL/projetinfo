cefdd9be5b7f59791510fd8493e1bdd31e92aa0e
Test01236546 <jeremie.otto@gmail.com>
2023-05-07 13:54:33 +0200

    Modification pour prendre en compte les tarifs

--- graphe.py
+++ graphe.py
@@ -25,7 +25,8 @@ class Graphe :
             #le noeud est ajouté en temps que clée et la valeur associée sera un set des noeuds adjacents 
     
     def add_edge(self,n_start,n_end,prix=1):
-        """Ajoute un arc au graphe, les noeuds peuvent déja exister
+        """Ajoute un arc au graphe, les noeuds peuvent déja exister,
+        tarif unique
 
         Parameters
         ----------
@@ -40,5 +41,33 @@ class Graphe :
         self.add_vertex(n_start)
         self.add_vertex(n_end)
         self.vertex[n_start].add(n_end)       #c'est pas grave si e n'est pas dans la liste des noeuds de s
-        self.edges[(n_start,n_end)]=prix      
+        self.edges[(n_start,n_end)]=prix
 
+    def add_edge2(self,n_start,n_end,nom_tarif,prix=1):
+        """Ajoute un arc au graphe, les noeuds peuvent déja exister,
+        les types de tarifs sont pris en compte
+
+        Parameters
+        ----------
+        n_start : int
+            noeud de départ de l'arc, id d'une gare
+        n_end : int
+            noeud d'arrivée de l'arc, id d'une gare
+        nom_tarif : str
+            le nom du tarif choisi
+        prix : float
+            le prix du billet
+
+        """
+        if (n_start,n_end) in list(self.edges.keys()):
+            self.edges[(n_start,n_end)][nom_tarif]=prix
+        else :
+            self.add_vertex(n_start)
+            self.add_vertex(n_end)
+            self.vertex[n_start].add(n_end)       
+            self.edges[(n_start,n_end)]={}
+            self.edges[(n_start,n_end)][nom_tarif]=prix
+      
+
+
+#MODIFIER add edge pour n'avoir qu'une valeur si plusieurs tarif pour meme destination
--- table_to_graphe.py
+++ table_to_graphe.py
@@ -18,3 +18,31 @@ def creation_graphe(dataframe):
     G=grp.Graphe()
     dataframe.apply(lambda row: G.add_edge(row['origine_code'], row['destination_code'], row['Prix']), axis=1) 
     return G
+
+def creation_graphe2(dataframe):
+    """Converti un dataframe départ/arrivée/prix en Graphe,
+    Les types de tarif sont pris en compte
+    
+    Parameters
+    ----------
+    dataframe : dataframe
+        un dataframe
+
+    Returns
+    ----------
+    Graphe : 
+        une instance de la classe Graphe  
+    """
+    G=Graphe()
+    dataframe.apply(lambda row: G.add_edge2(row['origine_code'], row['destination_code'],row['Type_tarif'], row['Prix']), axis=1) 
+    return G
+
+# dict2 = {'origine_code':[87751321,87751321,87751321],
+#          'destination_code':[87751750,87751438,87751750],
+#          'Type_tarif':['Tarif normal','Tarif normal','Tarif spécial'],
+#          'Prix':[21.8,15.2,50.4]}
+
+# df2=pd.DataFrame(dict2)
+# G=creation_graphe(df2)
+# G.edges
+# G=creation_graphe2(df2)
