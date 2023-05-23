f1d6c97d6ee8ca6693223b5684db60c4634c6ba8
CamilleNAVEL <129167200+CamilleNAVEL@users.noreply.github.com>
2023-05-23 12:01:32 +0200

    Update test.py

--- Dijkstra/test.py
+++ Dijkstra/test.py
@@ -1,11 +1,12 @@
 from dijkstra_source_vers_destination import dijkstra_source_vers_destination
 import pandas as pd
 
-graphe = pd.DataFrame({'origine' : ["PARIS ","PARIS","LYON"],
-                     'code_origine' : ['1','1','2'],
-                     'destination' : ["LYON ","MARSEILLE","MARSEILLE"],
-                     'code_destination' : ['2','3','3'],
-                     'prix' : [50,100,30]}
-                    )
+graphe = pd.DataFrame({
+    'origine': ["PARIS", "PARIS", "LYON"],
+    'code_origine': ['1', '1', '2'],
+    'destination': ["LYON", "MARSEILLE", "MARSEILLE"],
+    'code_destination': ['2', '3', '3'],
+    'prix': [50, 100, 30]
+})
 
-dijkstra_source_vers_destination(graphe,"PARIS","MARSEILLE")
+dijkstra_source_vers_destination(graphe, "PARIS", "MARSEILLE")
