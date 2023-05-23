24ecbe35b49ac291f3c36fb7dae6b117871dd9a9
CamilleNAVEL <129167200+CamilleNAVEL@users.noreply.github.com>
2023-05-23 12:42:56 +0200

    Update dijkstra_source_vers_tous.py

--- Dijkstra/dijkstra_source_vers_tous.py
+++ Dijkstra/dijkstra_source_vers_tous.py
@@ -1,27 +1,11 @@
-import heapq
+import pandas as pd
+import numpy as np
+from dijkstra_source_vers_destination import dijkstra_source_vers_destination
 
-def dijkstra_vers_tous(self, origine):
-    """
-    Dans cet algorithme, G représente le graphe sous forme d'un dictionnaire où chaque clé est un sommet et chaque valeur est un dictionnaire des sommets adjacents
-     et des poids des arêtes correspondantes. sdeb est le sommet de départ. La fonction retourne un dictionnaire d des distances les plus courtes de sdeb à chaque sommet
-    sdans G.
-    """
-    # Initialisation des distances à l'infini, sauf pour le sommet de départ
-    d = {s: float('inf') for s in self}
-    d[origine] = 0
-    
-    # Initialisation de la file de priorité avec le sommet de départ
-    pq = [(0, origine)]
-    
-    while pq:
-        # Extraction du sommet avec la plus petite distance estimée
-        dist, u = heapq.heappop(pq)
-        
-        # Mise à jour des distances pour tous les sommets accessibles depuis u
-        for v, w_uv in G[u].items():
-            alt = dist + w_uv
-            if alt < d[v]:
-                d[v] = alt
-                heapq.heappush(pq, (alt, v))
-    
-    return d
+
+def dijkstra_source_vers_tous(self, origine):
+
+    for destinations in np.unique(self.destination):
+
+        print("\nChemin optimal vers" ,destinations)
+        dijkstra_source_vers_destination(self,origine,destination= destinations)
